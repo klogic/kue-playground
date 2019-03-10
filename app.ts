684b62b43da64075bb03922224ef784bb8515d08
Narongsak Keawmanee <klogic@hotmail.co.th>
2019-03-10 23:35:18 +0700

    job processing and show result

--- app.ts
+++ app.ts
@@ -1,16 +1,17 @@
 import { Response, Request } from "express";
+import { Job, DoneCallback } from "kue";
+import axios from "axios";
+
 const kue = require("kue");
 const express = require("express");
 const app = express();
 const port = 3000;
-const axios = require("axios");
-
 const queue = kue.createQueue();
 
 app.get("/", (req: Request, res: Response) => {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 1; i <= 20; i++) {
     queue
-      .create("queue example " + i, {
+      .create("queue example", {
         title: "This testing request",
         data: i
       })
@@ -19,7 +20,16 @@ app.get("/", (req: Request, res: Response) => {
   }
   res.send("Hello World!");
 });
-
+queue.process("queue example", (job: Job, done: DoneCallback) => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos/" + job.data.data)
+    .then(result => {
+      console.log(result.data);
+      done();
+      return result.data;
+    })
+    .catch(error => done(error));
+});
 app.use("/kue-api/", kue.app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
